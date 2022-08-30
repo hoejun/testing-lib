7ae0fcf01b2d4ad7d1f2a3b797128faae1d28d81
hoejun <hjkim.it@gmail.com>
2022-08-30 15:15:57 +0900

    no message

--- src/components/Todo.test.js
+++ src/components/Todo.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+//cleanup X
 import { render, screen, fireEvent, getByLabelText, getByText } from '@testing-library/react';
 import Todo from './Todo';
 
@@ -49,6 +50,7 @@ import Todo from './Todo';
 //   });
 // });
 
+//테스트 케이스1
 test('home라는 텍스트가 있다.', () => {
   render(<Todo />);
 
@@ -57,9 +59,17 @@ test('home라는 텍스트가 있다.', () => {
   expect(text).toBeInTheDocument();
 });
 
+//테스트 케이스2
 test('input 태그의 텍스트가 제대로 입력되어있다.', () => {
+  //render는 구조분해하거나, wrapper 하지하지말것.
+  //1.
   const { getByTestId, getByText } = render(<Todo />);
   const fieldNode = getByTestId('form-field');
+
+  //2.
+  //screen을 사용하면 구조분해를 할 필요없음
+  // render(<Todo />);
+  // const testId = screen.getByTestId('form-field');
 
   //form 테스트
   const newTask = 'testing';
@@ -73,15 +83,11 @@ test('input 태그의 텍스트가 제대로 입력되어있다.', () => {
   //screen.getByText(/hello world/i); -> 태그 안에 있는 text를 기준으로 element를 선택, 다른 element를 선택할 확률이 있음
   //screen.getByTestId('helloWorld'); -> data-testid 속성의 값을 기준으로 선택 되기 때문에 안정성 높음.
   const btnNode = getByTestId('form-btn');
+  //screen.getByRole('button', {name:/submit/i})
   fireEvent.click(btnNode);
 
+  //getByText는 사용하지말것.
   const tdNode = getByText(newTask);
+  //toBe->toBeDisabled() 단언문을 사용할것. jest-dom
   expect(tdNode).toBeDefined();
-  // const onSubmit = jest.fn();
-  // const inputValue = '숫자 텍스트';
-
-  // fireEvent.change(getByLabelText(/message/i), { target: { value: inputValue } });
-  // fireEvent.click(getByText(/확인/i));
-
-  // expect(onSubmit).toBeCalled();
 });
